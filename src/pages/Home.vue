44d50195c06ed08b771d2f5cb4836d0047788cbc
sjoongh <az45687@naver.com>
2025-03-31 23:43:27 +0900

    init

---
+++ src/pages/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <ScheduleList />
+  </div>
+</template>
+
+<script>
+import ScheduleList from '@/components/page/ScheduleList.vue';
+
+export default {
+  components: {
+    ScheduleList,
+  },
+};
+</script>
